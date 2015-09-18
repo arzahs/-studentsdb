25b4a6e19644017c0d5337850cf7c0a97e210afb
Sergey Nelepa <nelepa1995@mail.ru>
2015-09-18 13:48:45 +0300

    add paginator for groups

--- students/views/group.py
+++ students/views/group.py
@@ -4,9 +4,19 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.template import RequestContext
 from students.models import Group
+from django.core.paginator  import Paginator, EmptyPage, PageNotAnInteger
 def groups_list(request):
 	groups = Group.objects.all()
+	paginator = Paginator(groups, 5)
+	page = request.GET.get('page')
+	try:
+		groups = paginator.page(page)
+	except PageNotAnInteger:
+		groups = paginator.page(1)
+	except EmptyPage:
+		groups = paginator.page(paginator.num_pages)
 	return render(request, 'students/groups_list.html', {"groups": groups})
+
 def groups_edit(request, sid):
 	return HttpResponse('group edit %s'% sid)
 
