4db115008582504e506b964e049949a22413236e
Sergey Nelepa <nelepa1995@mail.ru>
2015-09-22 20:48:23 +0300

    added widget calendar

--- static/js/main.js
+++ static/js/main.js
@@ -51,7 +51,16 @@ else {
 
 }
 
+function initDateFields() {
+$('input.dateinput').datetimepicker({
+'format': 'YYYY-MM-DD'
+}).on('dp.hide', function(event){
+  $(this).blur();
+});
+}
+
 $(document).ready(function(){
   initJournal();
   initGroupSelector();
+  initDateFields();
 });
